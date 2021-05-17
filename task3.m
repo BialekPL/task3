4fc7b5e3980e57d5ffe3bff044f21d115b8c8698
BialekPL <mbialecki157@gmail.com>
2021-05-17 10:32:31 +0200

    zrobione z małym oszukaniem

--- task3.m
+++ task3.m
@@ -24,17 +24,50 @@ hold off
 centroidy2 = vertcat(centroidy.Centroid);
 [bandwidth,density,X,Y] = kde2d(centroidy2);
 figure(4); 
-imshow(obraz1);title("mapa gęstości")
+imshow(obraz1);title("mapa gęstości wszystkich komórek")
 hold on;
 contour(X,Y,density,50);
 hold off;
 
 %% Task3B
 obraz2 = imread("oran.tif");
+obraz2 = obraz2(:,:,1);
 binaryzacja = imbinarize(obraz2, 'global');
 binaryzacja = binaryzacja(:,:,1);
 binaryzacja = imfill(binaryzacja, 'holes');
-dylacja = imdilate(binaryzacja, strel('disk',6,4));
-figure(5);imshow(dylacja)
-figure(6);imshow(dylacja - binaryzacja)
+binaryzacja = bwareaopen(binaryzacja, 20);
+dylacja = imdilate(binaryzacja, strel('disk',8,4));
+ringi = dylacja - binaryzacja;
+figure(5);imshow(ringi); title("ringi")
+limfocyty = zeros(3433,3488);
 
+%dla każdego piksela, jeśli otoczenie w dapi+ring jest intesywniejsze niż
+%otoczenie w samym dapi, to wyświetlany jest piksel z dapi. Jeśli nie,
+%wyświetlany jest czarny piksel
+dapi_i_ringi = obraz_szary + uint8(ringi);
+dir = dapi_i_ringi;
+for i = 2:3432
+    for j = 2:3487
+        otoczenie_dir = dir(i-1,j-1) + dir(i-1, j) + dir(i-1, j+1) + dir(i,j-1 ) + dir(i, j) + dir(i, j+1) + dir(i+1, j-1) + dir(i+1, j) + dir(i+1, j+1);
+        otoczenie_dapi = obraz_szary(i-1,j-1) + obraz_szary(i-1, j) + obraz_szary(i-1, j+1) + obraz_szary(i,j-1 ) + obraz_szary(i, j) + obraz_szary(i, j+1) + obraz_szary(i+1, j-1) + obraz_szary(i+1, j) + obraz_szary(i+1, j+1);
+        if otoczenie_dir > otoczenie_dapi
+            limfocyty(i,j) = obraz_szary(i,j);
+        end
+    end
+end
+
+limfocyty_wypelnione = imfill(limfocyty);
+limfocyty_oczyszczone = bwareaopen(limfocyty_wypelnione, 60);
+figure(6);imshow(limfocyty_oczyszczone);title("Maska binarna limfocytów uzyskana z obrazu DAPI");
+
+% przekształcenie do rgb by heatmapa wyświetlała się w kolorze
+limfocyty_rgb = cat(3, limfocyty_wypelnione, limfocyty_wypelnione, limfocyty_wypelnione);
+
+[output_image2, liczba_jader2, binarna_maska2, centroidy3] = nuclei_counter((im2gray(obraz2)));
+centroidy4 = vertcat(centroidy3.Centroid);
+[bandwidth2,density2,X2,Y2] = kde2d(centroidy4);
+figure(7); 
+imshow(limfocyty_rgb);title("mapa gęstości limfocytów CD8")
+hold on;
+contour(X,Y,density2,50);
+hold off;
